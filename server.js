30c10c1c64a3b1db1fb2be0f00a5681b27a1e198
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2017-02-14 01:02:39 +0530

    passport-jwt working but still in the server file

--- server.js
+++ server.js
@@ -4,7 +4,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+var jwt = require('jsonwebtoken');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+var passport = require('passport');
+
+var jwtStrategy = require('passport-jwt').Strategy;
+var extractJwt = require('passport-jwt').ExtractJwt;
+var opts = {};
+
+opts.jwtFromRequest = extractJwt.fromAuthHeader();
+opts.secretOrKey = 'secret';
+passport.use(new jwtStrategy(opts,function(jwt_payload,done){
+    console.log("in the jwt strategy");
+    done(null,"a");
+}));
+
+app.post('/login',function(req,res){
+    if(req.body.username==="test" && req.body.password==="test"){
+        var myToken = jwt.sign({username:req.body.username},'secret');
+        res.json({token:myToken});
+    }
+})
+
+app.post('/profile',passport.authenticate('jwt',{session:false}),
+            function(req,res){
+                res.json("access the profile");
+            });
 
 var database = require('./config/database');
 var port = require('./config/config').port;
